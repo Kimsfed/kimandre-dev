7905a61a3e3beafedc1fc60136dd4dbbb723f71a
unknown <kimandre.aadland@gmail.com>
2023-08-25 00:38:34 +0200

    Second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,30 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Inter, Open_Sans } from "next/font/google"
+import styles from "./styles/variables.module.scss"
+
+
+const inter = Inter({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+const openSans = Open_Sans({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <header>
+      <h1 className={`${inter.className} ${styles.PrimaryDarkTextColor}`} > Kim Andre Dev</h1>
+      <ul className={`${styles.openSans}`} >
+        <li ><a href="/test">About me</a></li>
+        <li><a href="/test">Contact</a></li>
+      </ul>
+        <div className={styles['checkbox-wrapper-34']}>
+          <input className={`${styles.tgl} ${styles['tgl-ios']}`} id='toggle-34' type='checkbox'></input>
+          <label className={styles['tgl-btn']} htmlFor='toggle-34'></label>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+    </header>
+  )}
---
+++ src/app/test/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Test() {
+    return(
+        <h2>Denne er ikke klar</h2>
+    )
+}
